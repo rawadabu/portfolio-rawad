4046a3134b879db390b4d17bc6bb63239623e510
rawadabu <rawadabusaleh0@gmail.com>
2023-11-07 14:18:33 +0200

    UI:Added base components 🎨

---
+++ portfolio/src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="backdrop-blue-sm absolute inset-0 flex items-center justify-center bg-slate-200/30">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
