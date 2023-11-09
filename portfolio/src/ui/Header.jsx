f440b369bf6d195fa28dacf483bb3a757e907e3a
rawadabu <rawadabusaleh0@gmail.com>
2023-11-09 17:41:54 +0200

    Added logo 💥

--- portfolio/src/ui/Header.jsx
+++ portfolio/src/ui/Header.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import Rawad from "../assets/Rawad.png";
 
 function Header() {
   return (
     <header className="sticky top-0 px-4 py-3 uppercase sm:px-6 shadow-lg font-playpen flex justify-between items-center bg-backGround text-textColor z-10">
-      <Link to="/" className="tracking-widest">
-        Rawad Abu-Saleh
-      </Link>
+      <div className="flex items-center">
+        <img
+          src={Rawad}
+          alt="Logo"
+          className="mr-2 h-14 w-14 hover:animate-bounce"
+        />
+        <Link to="/" className="tracking-widest">
+          Rawad Abu-Saleh
+        </Link>
+      </div>
       <div className="flex justify-end gap-5">
         <Button type="navbar">About</Button>
         <Button type="navbar">Skills</Button>
