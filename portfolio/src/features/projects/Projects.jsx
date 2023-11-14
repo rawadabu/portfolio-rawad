fb61f8356a7ec4e63e4da0e22ce2893b03750abf
rawadabu <rawadabusaleh0@gmail.com>
2023-11-14 14:03:38 +0200

    Tailwind: updated the loader css

--- portfolio/src/features/projects/Projects.jsx
+++ portfolio/src/features/projects/Projects.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import { useLoaderData } from "react-router-dom";
 import { getSections } from "../../services/apiRepos";
 import Button from "../../ui/Button";
-import TopProjects from "./topProjects";
+import TopProjects from "./TopProjects";
 
 function Projects() {
   const sections = useLoaderData();
